29ad547d178d607c9e649f4c571f5aa5279462c5
Dallin <Dalekdallin256@gmail.com>
2025-02-15 22:58:19 -0500

    card array implimented

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,22 @@ const initialCards= [
       name: "Aoraki Mountain ",
       link: "https://images.unsplash.com/photo-1589023846998-3cbff59d037f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
    }, 
-   {name: "Stone Forest" ,
+   {  name: "Stone Forest" ,
       link: "https://plus.unsplash.com/premium_photo-1661963421878-fbe18ea938f1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
    }, 
-   { name: "Hagia Sophia" ,
+   {  name: "Hagia Sophia" ,
       link: "https://images.unsplash.com/photo-1623621534850-d325a1980c7e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-   }, {name: "Rainbow Row" ,
+   }, 
+   {  name: "Rainbow Row" ,
       link: "https://images.unsplash.com/photo-1588007129936-5b7754628cd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNoYXJsZXN0b258ZW58MHx8MHx8fDA%3D"
-   }, {name: "Thai Market" ,
+   }, 
+   {  name: "Thai Market" ,
       link: "https://images.unsplash.com/photo-1614888901558-dd1ef11bada3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-   }, {name: "Congaree" ,
+   }, 
+   {  name: "Congaree" ,
       link: "https://images.unsplash.com/photo-1647747836228-e4a823814697?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29uZ2FyZWV8ZW58MHx8MHx8fDA%3D"
-   }];
+   }
+];
 
 const profileEdit = document.querySelector("#profile-edit", "modal");
 const profileEditButton = document.querySelector("#profile-edit-button") ;
@@ -61,9 +65,10 @@ function cardSet(data){
 };
 
    function addCards(){for (const icard of initialCards) {
-      cardSet(icard);
-      cardsBox.append(icard);
+      const cardEl = cardSet(icard);      
+      cardsBox.append(cardEl);
    }}
 
-   addCards();
+addCards();
+
 
